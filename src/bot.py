3340632a07ba907fc6ff836c41ed9ff8bf1138f0
Maryam Mohebbi <maryam.mohebbi.2@gmail.com>
2023-01-10 23:23:20 +0100

    create /getPrice

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,8 @@
 # from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 import os
 import requests
+from telegram.ext import filters, MessageHandler
 
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -46,10 +47,40 @@ async def help(update, context):
 Available commands:
 /help - List of available commands
 /start - Get rate of top currencies
+/getPrice - Select a coin and get its price
         '''
     )
 
 
+async def get_price(update, context):
+    await update.message.reply_text(
+        f''' Which currency rate do you need?'''
+    )
+
+
+async def find_price(update, context):
+    currency_name = update.message.text
+    currency_name = currency_name.upper()
+    endpoint = f"exchangerate/{currency_name}/USD"
+    try:
+        response = requests.get(api_url + endpoint, headers=api_headers).json()
+        name = response["asset_id_base"]
+        price = response["rate"]
+        price_formatted = "${:,.2f}".format(price)
+        await update.message.reply_text(
+            f"The price of {name} is {price_formatted}."
+        )
+    except:
+        await update.message.reply_text(
+            f"Invalid currency name entered."
+        )
+
+
 builder.add_handler(CommandHandler('start', start))
 builder.add_handler(CommandHandler('help', help))
+builder.add_handler(CommandHandler('getPrice', get_price))
+find_price_handler = MessageHandler(
+    filters.TEXT & (~filters.COMMAND), find_price)
+builder.add_handler(find_price_handler)
+
 builder.run_polling()
